08d82e60465a6a9e5bf1aea9ded94331a32d32ba
Ewere Diagboya <nimboya@gmail.com>
2024-02-08 10:44:36 +0100

    update php ui

--- index.php
+++ index.php
@@ -1,3 +1,64 @@
-<?php
-echo "Welcome to PHP on PipeOps!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PipeOps Template</title>
+    <link rel="stylesheet" href="css/app.css" />
+  </head>
+  <body>
+    <header><img src="images/pipeops-light.svg" alt="pipeops logo" /></header>
+    <main>
+      <section class="hero">
+        <h1>Your <img src="images/php-1.svg" alt="PHP logo"/> deployment is successful 🎉</h1>
+        <p>
+          Now that your deployment is successful, there's a whole lot
+          more you can explore on PipeOps.
+        </p>
+      </section>
+      <section>
+        <div class="cardsWrapper">
+          <a target="_blank" href="https://docs.pipeops.io/" class="card">
+            <div><img src="images/book.svg" alt="book" />
+            <h2>Documentation</h2></div>
+            <p>Full details on what PipeOps has to offer</p>
+          </a>
+          <a target="_blank" href="https://pipeops.io/resources/guides" class="card">
+           <div> <img src="images/layers.svg" />
+            <h2>Guides</h2></div>
+            <p>Get Started without stress</p>
+          </a>
+        <a target="_blank" href="https://pipeops.io/resources/tutorials" class="card">
+            <div>
+              <img src="images/bookmark.svg" />
+            <h2>Tutorials</h2>
+            </div>
+            <p>How-tos from the basic to most advanced</p>
+          </a>
+          <a target="_blank" href="https://join.slack.com/t/pipeopscommunity/shared_invite/zt-23gmjrl0k-Pzm2cBgIMTsUu5Az73PYKg" class="card">
+            <div>
+              <img src="images/slack.svg" />
+            <h2>Slack</h2>
+            </div>
+            <p>Join our community on Slack</p>
+          </a>
+          <a target="_blank" href="https://discord.gg/PQscGtfFD2" class="card">
+            <div>
+              <img src="images/message-circle.svg" />
+            <h2>Discord</h2>
+            </div>
+            <p>Join our community on Discord and stay up to date with the latest on Pipeops</p>
+          </a>
+        </div>
+      </section>
+    </main>
+    <footer>
+      <p>© PipeOps -<a href="https://pipeops.io"> PipeOps.io</a> -
+        <a target="_blank" href="/">Github</a> -
+        <a target="_blank" href="https://discord.gg/PQscGtfFD2">Discord</a> -
+        <a target="_blank" href="https://join.slack.com/t/pipeopscommunity/shared_invite/zt-23gmjrl0k-Pzm2cBgIMTsUu5Az73PYKg">Slack</a> -
+        <a target="_blank" href="https://blog.pipeops.io/">Blog</a>
+      </p>
+    </footer>
+  </body>
+</html>
